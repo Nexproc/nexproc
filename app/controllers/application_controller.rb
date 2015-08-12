c230138323ad40219693901536f74f2ca96e541e
Nexproc <ntitcombe@gmail.com>
2015-08-12 16:53:19 -0700

    fixed routing issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def login!(user)
     user.reset_session!
     session[:session_token] = user.session_token
-    redirect_to root
+    redirect_to root_url
   end
 
   private
