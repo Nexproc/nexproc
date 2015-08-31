11c0e15d19ca4595356ab58811793e67535aeadd
Nexproc <ntitcombe@gmail.com>
2015-08-31 10:14:06 -0700

    removed janky animations

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -5,9 +5,8 @@ Nexproc.Routers.Router = Backbone.Router.extend({
     this.teams = new Nexproc.Collections.Teams();
     this.projects = new Nexproc.Collections.Projects();
     this.tasks = new Nexproc.Collections.Tasks();
-    // this.teams.fetch();
-    // this.projects.fetch();
-    // this.tasks.fetch();
+    this._mainViewSwitch = true;
+    this._subViewSwitch = true;
   },
 
   routes: {
@@ -122,34 +121,18 @@ Nexproc.Routers.Router = Backbone.Router.extend({
   _switchSubView: function (view) {
     this._currentSubView && this._currentSubView.remove();
     this._currentSubView = view;
-    this.$subEl.html(view.render().$el);
+    this.$subEl.prepend(view.render().$el);
   },
 
   _switchMainView: function (view) {
-    var cback = function () {};
-    if(!this._currentMainView) {
-      cback = function(v) { v.$el.animate({"marginLeft": '+=150%', 'marginRight': '-=150%'}, 500); };
-    }
     this._removeViews();
     this._currentMainView = view;
-    this.$mainEl.append(view.render().$el);
-    view.$el.animate({"marginLeft": '-=150%', 'marginRight': '+=150%'}, 0);
-    cback(view);
+    this.$mainEl.html(view.render().$el);
   },
 
   _removeViews: function () {
     this._currentSubView && this._currentSubView.remove();
-    this._currentMainView && this._mainRemoveHandler();
-  },
-
-  _mainRemoveHandler: function (view) {
-    var that = this;
-    var oldView = this._currentMainView;
-    var removal = function () {
-      that._currentMainView.$el.animate({"marginLeft": '+=150%', 'marginRight': '-=150%'}, 500);
-      oldView.remove();
-    };
-    oldView.$el.animate({"marginLeft": '-=150%', 'marginRight': '+=150%'}, {duration: 500, complete: removal});
+    this._currentMainView && this._currentMainView.remove();
   },
 
   _subViewHelper: function (keepView, cback) {
