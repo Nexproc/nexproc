79a7f2b21b7986349fb92409ddb68ae1bf4302d1
Nexproc <ntitcombe@gmail.com>
2015-09-07 09:24:12 -0700

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,27 @@ guest.save
 
 #seed fake users
 100.times do
-  user = User.new(
-    username: Faker::Name.first_name,
-    password: "password"
-    )
-  user.save
+  i = 1
+  while i < 100
+    user = User.new(
+      username: Faker::Name.first_name,
+      password: "password"
+      )
+    i += 1 if user.save
+  end
 end
 
 #seed teams
 20.times do
-  user_ids = (2..100).to_a
+  #make team
   team = Team.new(name: Faker::App.name)
   team.save
   team.members << guest
 
-  #populate team member
+  #populate team members
+  user_ids = (2..100).to_a.shuffle
   (rand(20) + 4).times do
-
-    #look for a valid user id
-    id = 0
-    until user_ids.include?(id)
-      id = rand(99) + 2
-    end
-    #remove id form valid list
-    user_ids.delete(id)
-
+    id = user_ids.pop
     team.members << User.find(id)
   end
   team.save
